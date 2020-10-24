cd114f3e937926a0c8895449195309163484586f
megs-nair <37657492+megs-nair@users.noreply.github.com>
2020-10-25 02:40:32 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,7 @@
+
+chrome.tabs.onCreated.addListener(function(){
+    //alert(document.body.style.background);
+    //document.body.style.backgroundColor = 'green';
+    //document.body.style.backgroundImage='https://image.shutterstock.com/z/stock-photo-white-transparent-leaf-on-mirror-surface-with-reflection-on-turquoise-background-macro-artistic-1029171697.jpg';
+
+});
